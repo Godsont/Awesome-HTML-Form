4d5abd92f01662fab3499bdd64588126f7c02881
Godson Thomas <godson.aocmusic@gmail.com>
2019-02-12 11:58:46 +0530

    First Commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const GTInput = document.querySelectorAll(".gt-input");
+
+for (let i = 0; i < GTInput.length; i++) {
+  let currentLabel = GTInput[i].parentElement.firstElementChild;
+
+  GTInput[i].addEventListener("focus", function () {
+    currentLabel.classList.add("move-up");
+  })
+
+  GTInput[i].addEventListener("blur", function () {
+    if (GTInput[i].value === "") {
+      currentLabel.classList.remove("move-up");
+    }
+  })
+}
